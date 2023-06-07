4c24b779e268d66ac2315a9c36ba6925867c5b71
ubiratanfilho <ubfilho16@gmail.com>
2023-06-07 15:02:57 -0300

    Conexão com banco requisição verify login

--- login/index.js
+++ login/index.js
@@ -12,13 +12,13 @@ app.post('/login', async (req, res) => {
   const client = new Client({
     host: "localhost",
     user: "postgres",
-    password: "Uj100000",
+    password: "UJ10000",
     database: "cormenai",
     port: 5432
   })
   await client.connect()
 
-
+  // requisição
   const { username, email, password } = req.body;
   const login = { username, email, password };
   res.status(201).send(login);
@@ -28,28 +28,60 @@ app.post('/login', async (req, res) => {
   await client.end()
 });
 
-// Get the login according to the id
-app.get('/login/:id', (req, res) => {
-  const { id } = req.params;
-  res.status(200).send(login[id]);
+// Get the login according to the username
+app.get('/login/:username', async (req, res) => {
+  const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    password: "UJ10000",
+    database: "cormenai",
+    port: 5432
+  })
+  await client.connect()
+
+  const { username } = req.params;
+
+  // consulta no banco de dados
+  try {
+    const result = await client.query(
+      `SELECT * FROM login WHERE username = $1`,
+      [username]
+    );
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error retrieving login from database');
+  }
+
+  await client.end()
 });
 
 // Verify the login
-app.post('/login/verify', (req, res) => {
+app.post('/login/verify', async (req, res) => {
+  const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    password: "UJ10000",
+    database: "cormenai",
+    port: 5432
+  })
+  await client.connect()
+
   const { email, password } = req.body;
-  let id = 1;
-  let found = false;
-  while (id <= Object.keys(login).length && !found) {
-    if (login[id].email === email && login[id].password === password) {
-      found = true;
-    } else {
-      id++;
+  try{
+    const result = await client.query(
+      `SELECT count(*) as qtd FROM login WHERE email = $1 AND password = $2`,
+      [email, password]
+    );
+    if (result.rows[0].qtd > 0) {
+      res.status(201).send(true);
     }
-  }
-  if (found) {
-    res.status(200).send(login[id]);
-  } else {  
-    res.status(404).send('Not found');
+    else {
+      res.status(201).send(false);
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error retrieving login from database');
   }
 });
 
