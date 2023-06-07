bc504f069f46a9baf0f323f834f70d1127cd9fe6
ubiratanfilho <ubfilho16@gmail.com>
2023-06-07 17:30:58 -0300

    feat: :sparkles: Barramento de eventos do microsserviço de login

--- login/index.js
+++ login/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { Client } = require('pg');
+const amqp = require('amqplib');
+
 const app = express();
 const port = 3001;
 
@@ -8,82 +10,95 @@ app.use(bodyParser.json());
 
 // Post the login
 app.post('/login', async (req, res) => {
-  // conexão com o banco de dados
-  const client = new Client({
-    host: "localhost",
-    user: "postgres",
-    password: "UJ10000",
-    database: "cormenai",
-    port: 5432
-  })
-  await client.connect()
-
-  // requisição
   const { username, email, password } = req.body;
   const login = { username, email, password };
-  res.status(201).send(login);
 
-  // inserção no banco de dados
-  client.query(`INSERT INTO login (username, email, password) VALUES ('${username}', '${email}', '${password}')`)
-  await client.end()
+  // send message to RabbitMQ queue
+  const connection = await amqp.connect('amqp://localhost');
+  const channel = await connection.createChannel();
+  const queueName = 'loginQueue';
+  await channel.assertQueue(queueName);
+  await channel.sendToQueue(queueName, Buffer.from(JSON.stringify(login)));
+  await channel.close();
+  await connection.close();
+
+  res.status(201).send(login);
 });
 
-// Get the login according to the username
-app.get('/login/:username', async (req, res) => {
+// consume messages from RabbitMQ queue and insert into database
+async function consumeMessages() {
   const client = new Client({
     host: "localhost",
     user: "postgres",
     password: "UJ10000",
     database: "cormenai",
     port: 5432
-  })
-  await client.connect()
+  });
+  await client.connect();
 
-  const { username } = req.params;
+  const queueName = 'loginQueue';
+  const connection = await amqp.connect('amqp://localhost');
+  const channel = await connection.createChannel();
+  await channel.assertQueue(queueName);
+  await channel.consume(queueName, async (message) => {
+    const login = JSON.parse(message.content.toString());
+    await client.query(`INSERT INTO login (username, email, password) VALUES ('${login.username}', '${login.email}', '${login.password}')`);
+    console.log(`Login inserted: ${JSON.stringify(login)}`);
+    channel.ack(message);
+  });
 
-  // consulta no banco de dados
-  try {
-    const result = await client.query(
-      `SELECT * FROM login WHERE username = $1`,
-      [username]
-    );
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error retrieving login from database');
-  }
+  console.log('Waiting for messages...');
+}
 
-  await client.end()
-});
+consumeMessages().catch(console.error);
 
 // Verify the login
 app.post('/login/verify', async (req, res) => {
+  const { username, password } = req.body;
+
+  // send message to RabbitMQ queue
+  const connection = await amqp.connect('amqp://localhost');
+  const channel = await connection.createChannel();
+  const queueName = 'loginVerifyQueue';
+  await channel.assertQueue(queueName);
+  await channel.sendToQueue(queueName, Buffer.from(JSON.stringify({ username, password })));
+  await channel.close();
+  await connection.close();
+
+  res.sendStatus(200);
+});
+
+// consume messages from RabbitMQ queue and verify the login
+async function consumeMessagesVerify() {
   const client = new Client({
     host: "localhost",
     user: "postgres",
     password: "UJ10000",
     database: "cormenai",
     port: 5432
-  })
-  await client.connect()
+  });
+  await client.connect();
 
-  const { email, password } = req.body;
-  try{
-    const result = await client.query(
-      `SELECT count(*) as qtd FROM login WHERE email = $1 AND password = $2`,
-      [email, password]
-    );
-    if (result.rows[0].qtd > 0) {
-      res.status(201).send(true);
+  const queueName = 'loginVerifyQueue';
+  const connection = await amqp.connect('amqp://localhost');
+  const channel = await connection.createChannel();
+  await channel.assertQueue(queueName);
+  await channel.consume(queueName, async (message) => {
+    const login = JSON.parse(message.content.toString());
+    const result = await client.query(`SELECT * FROM login WHERE username = '${login.username}' AND password = '${login.password}'`);
+    if (result.rows.length > 0) {
+      console.log(`Login verified: ${JSON.stringify(login)}`);
+    } else {  
+      console.log(`Login not verified: ${JSON.stringify(login)}`);
     }
-    else {
-      res.status(201).send(false);
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error retrieving login from database');
-  }
-});
+    channel.ack(message);
+  });
+
+  console.log('Waiting for messages...');
+}
+
+consumeMessagesVerify().catch(console.error);
+
 
 app.listen(port, () => {
   console.log(`Microservice listening at http://localhost:${port}`);
