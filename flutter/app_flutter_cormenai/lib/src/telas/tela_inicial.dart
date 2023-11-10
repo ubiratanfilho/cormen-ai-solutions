67a6bd87c5fa873108a6b3a213e9d537364b76d4
SocioPJ <sociopj45@outlook.com>
2023-11-10 16:21:52 -0300

    feat: :sparkles: Header Pagina Inicial

--- flutter/app_flutter_cormenai/lib/src/telas/tela_inicial.dart
+++ flutter/app_flutter_cormenai/lib/src/telas/tela_inicial.dart
@@ -2,11 +2,15 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:logging/logging.dart';
+import 'login_tela.dart';
+import 'registrar_tela.dart';
+import 'registrar_noticias.dart';
+
 
 
 
 class TelaInicial extends StatelessWidget {
-  final Logger _logger = Logger('TelaInicial');
+  final Logger _logger = Logger('CormenAI Solutions');
 
   TelaInicial({Key? key}) : super(key: key);
 
@@ -22,7 +26,40 @@ class TelaInicial extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<dynamic>>(
+    return Scaffold(
+    appBar: AppBar(
+      title: Text('Tela Inicial'),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.pageview),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => LoginTela()),  // Substitua Pagina1 pela sua classe de página
+            );
+          },
+        ),
+        IconButton(
+          icon: Icon(Icons.navigate_next),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => RegistrarUsuarioTela()),  // Substitua Pagina2 pela sua classe de página
+            );
+          },
+        ),
+        IconButton(
+          icon: Icon(Icons.navigate_next),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => RegistrarNoticiasTela()),  // Substitua Pagina2 pela sua classe de página
+            );
+          },
+        ),
+      ],
+    ),   
+    body: FutureBuilder<List<dynamic>>(
       future: fetchNoticias(),
       builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
         if (snapshot.hasData) {
@@ -48,7 +85,7 @@ class TelaInicial extends StatelessWidget {
           return CircularProgressIndicator();
         }
       },
-    );
-
+      ),
+    ); 
   }
 }
