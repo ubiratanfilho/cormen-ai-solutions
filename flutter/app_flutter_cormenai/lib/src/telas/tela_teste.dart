379985ecace81f746c7d9fcda1042a1bcded3f2b
SocioPJ <sociopj45@outlook.com>
2023-11-16 20:50:03 -0300

    feat: :sparkles: Creating Buttons

--- flutter/app_flutter_cormenai/lib/src/telas/tela_teste.dart
+++ flutter/app_flutter_cormenai/lib/src/telas/tela_teste.dart
@@ -11,13 +11,54 @@ class TelaTeste extends StatelessWidget{
   Widget Header(){
     return Scaffold(
       appBar: AppBar(
-        title: Text("CormenAI Solutions",
-        style: TextStyle(color: Colors.white, fontSize: 20),),
-        backgroundColor: Color.fromARGB(255, 90, 15, 134),  // Altera a cor do AppBar para roxo
+        backgroundColor: Color.fromARGB(255, 90, 15, 134), 
         leading: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Image.asset('images/logo.png'),  // Adiciona o logo ao lado esquerdo do título
+          child: Image.asset('images/logo.png'), 
         ),
+        title: Stack(
+  alignment: Alignment.center,  // Centraliza os botões
+  children: <Widget>[
+    Align(
+      alignment: Alignment.centerLeft,
+      child: Text("CormenAI Solutions",
+        style: TextStyle(color: Colors.white, fontSize: 20),
+      ),
+    ),
+    Row(
+      mainAxisSize: MainAxisSize.min,  // Faz a Row tão pequena quanto possível
+      children: <Widget>[
+        TextButton(
+          child: Text(
+            'Botão 1', 
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,  // Altera o tamanho da fonte
+              fontFamily: 'Raleway-Light',  // Altera a fonte
+            ),
+          ),
+          onPressed: () {
+            // Adicione a ação do botão 1 aqui
+          },
+        ),
+        SizedBox(width: 20),
+        TextButton(
+          child: Text(
+            'Botão 2', 
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,  // Altera o tamanho da fonte
+              fontFamily: 'Raleway-Light',  // Altera a fonte
+            ),
+          ),
+          onPressed: () {
+            // Adicione a ação do botão 2 aqui
+          },
+        ),
+      ],
+    ),
+  ],
+),
       ),
     );
   }
