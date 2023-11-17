63e25dfa24d79636bd6e06322cc9bbf17e797e3b
SocioPJ <sociopj45@outlook.com>
2023-11-16 22:16:41 -0300

    feat: :lipstick: Home Body on constructing

--- flutter/app_flutter_cormenai/lib/src/telas/tela_teste.dart
+++ flutter/app_flutter_cormenai/lib/src/telas/tela_teste.dart
@@ -7,177 +7,182 @@ import 'registrar_tela.dart';
 import 'registrar_noticias.dart';
 
 class TelaTeste extends StatelessWidget {
-
-  // Widget Header() {
-    
-  // }
-
-  // Widget Noticias(){
-  //   return Container(
-  //   )
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 90, 15, 134),
-        leading: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Image.asset('images/logo.png'),
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 90, 15, 134),
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/logo.png'),
+          ),
+          title: Stack(
+            alignment: Alignment.center, // Centraliza os botões
+            children: <Widget>[
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "CormenAI Solutions",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+              LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraints) {
+                  if (constraints.maxWidth > 600) {
+                    return Row(
+                      mainAxisSize: MainAxisSize
+                          .min, // Faz a Row tão pequena quanto possível
+                      children: <Widget>[
+                        TextButton(
+                          child: Text(
+                            'Entrar',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16, // Altera o tamanho da fonte
+                              fontFamily: 'Raleway-Light', // Altera a fonte
+                            ),
+                          ),
+                          onPressed: () {
+                            // Adicione a ação do botão 1 aqui
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginTela()),
+                            );
+                          },
+                        ),
+                        SizedBox(width: 20), // Adiciona espaço entre os botões
+                        TextButton(
+                          child: Text(
+                            'Cadastrar',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16, // Altera o tamanho da fonte
+                              fontFamily: 'Raleway-Light', // Altera a fonte
+                            ),
+                          ),
+                          onPressed: () {
+                            // Adicione a ação do botão 2 aqui
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => RegistrarUsuarioTela()),
+                            );
+                          },
+                        ),
+                        SizedBox(width: 20), // Adiciona espaço entre os botões
+                        TextButton(
+                          child: Text(
+                            'Publicar',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16, // Altera o tamanho da fonte
+                              fontFamily: 'Raleway-Light', // Altera a fonte
+                            ),
+                          ),
+                          onPressed: () {
+                            // Adicione a ação do botão 3 aqui
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      RegistrarNoticiasTela()),
+                            );
+                          },
+                        ),
+                      ],
+                    );
+                  } else {
+                    return Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        TextButton(
+                          child: Text(
+                            'Entrar',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontFamily: 'Raleway-Light',
+                            ),
+                          ),
+                          onPressed: () {
+                            // Adicione a ação do botão 1 aqui
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginTela()),
+                            );
+                          },
+                        ),
+                        SizedBox(height: 20),
+                        TextButton(
+                          child: Text(
+                            'Cadastrar',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontFamily: 'Raleway-Light',
+                            ),
+                          ),
+                          onPressed: () {
+                            // Adicione a ação do botão 2 aqui
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => RegistrarUsuarioTela()),
+                            );
+                          },
+                        ),
+                        TextButton(
+                          child: Text(
+                            'Publicar',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontFamily: 'Raleway-Light',
+                            ),
+                          ),
+                          onPressed: () {
+                            // Adicione a ação do botão 2 aqui
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      RegistrarNoticiasTela()),
+                            );
+                          },
+                        ),
+                      ],
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
         ),
-        title: Stack(
-          alignment: Alignment.center, // Centraliza os botões
-          children: <Widget>[
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "CormenAI Solutions",
-                style: TextStyle(color: Colors.white, fontSize: 20),
+        body: Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 100.0),
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.all(35),
+              color: const Color.fromARGB(
+                  100, 92, 92, 92), // Define a cor de fundo para cinza claro
+              child: Align(
+                alignment: Alignment.center,
+                child: Container(
+                  width: 800,
+                  padding: EdgeInsets.all(35),
+                  decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 51, 51, 51),
+                      borderRadius: BorderRadius.circular(20)
+                  ),
+                  // child: Noticias(),
+                ),
               ),
             ),
-            LayoutBuilder(
-              builder: (BuildContext context, BoxConstraints constraints) {
-                if (constraints.maxWidth > 600) {
-                  return Row(
-                    mainAxisSize: MainAxisSize
-                        .min, // Faz a Row tão pequena quanto possível
-                    children: <Widget>[
-                      TextButton(
-                        child: Text(
-                          'Entrar',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16, // Altera o tamanho da fonte
-                            fontFamily: 'Raleway-Light', // Altera a fonte
-                          ),
-                        ),
-                        onPressed: () {
-                          // Adicione a ação do botão 1 aqui
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => LoginTela()),
-                          );
-                        },
-                      ),
-                      SizedBox(width: 20), // Adiciona espaço entre os botões
-                      TextButton(
-                        child: Text(
-                          'Cadastrar',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16, // Altera o tamanho da fonte
-                            fontFamily: 'Raleway-Light', // Altera a fonte
-                          ),
-                        ),
-                        onPressed: () {
-                          // Adicione a ação do botão 2 aqui
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => RegistrarUsuarioTela()),
-                          );
-                        },
-                      ),
-                      SizedBox(width: 20), // Adiciona espaço entre os botões
-                      TextButton(
-                        child: Text(
-                          'Publicar',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16, // Altera o tamanho da fonte
-                            fontFamily: 'Raleway-Light', // Altera a fonte
-                          ),
-                        ),
-                        onPressed: () {
-                          // Adicione a ação do botão 3 aqui
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => RegistrarNoticiasTela()),
-                          );
-                        },
-                      ),
-                    ],
-                  );
-                } else {
-                  return Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      TextButton(
-                        child: Text(
-                          'Entrar',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontFamily: 'Raleway-Light',
-                          ),
-                        ),
-                        onPressed: () {
-                          // Adicione a ação do botão 1 aqui
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => LoginTela()),
-                          );
-                        },
-                      ),
-                      SizedBox(height: 20),
-                      TextButton(
-                        child: Text(
-                          'Cadastrar',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontFamily: 'Raleway-Light',
-                          ),
-                        ),
-                        onPressed: () {
-                          // Adicione a ação do botão 2 aqui
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => RegistrarUsuarioTela()),
-                          );
-                        },
-                      ),
-                      TextButton(
-                        child: Text(
-                          'Publicar',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontFamily: 'Raleway-Light',
-                          ),
-                        ),
-                        onPressed: () {
-                          // Adicione a ação do botão 2 aqui
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => RegistrarNoticiasTela()),
-                          );
-                        },
-                      ),
-                    ],
-                  );
-                }
-              },
-            ),
-          ],
-        ),
-      ),
-      body: Center(
-        child: Container(
-          width: 600,  // Define a largura do Container
-          padding: EdgeInsets.all(35),
-          color: const Color.fromARGB(255, 92, 92, 92),  // Define a cor de fundo para cinza claro
-          child: Noticias(),  // Adicione o widget Noticias aqui
-        ),
-      )
-    );
-    
-  
-}
+          ),
+        ));
+  }
 }
